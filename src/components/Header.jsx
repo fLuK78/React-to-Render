e5dd5049d250184eda8616e4c9bc642ee6eb4963
ReewHup <taweesak99924@gmail.com>
2025-08-06 00:30:33 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <>
+        <header className='bg-black text-white'>
+            <div className='container-lg py-2'></div>
+            <h1>I LOVE YOU NAAAAAA</h1>
+            
+        </header>
+    </>
+  )
+}
+
+export default Header
